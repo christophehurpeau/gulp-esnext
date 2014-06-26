ee270f319081076a690d09a0a5e877e568732f62
Christophe Hurpeau <christophe@evaneos.com>
2014-06-26 10:43:07 +0200

    gulp-sourcemaps

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
 var gutil = require('gulp-util');
 var through = require('through2');
 var esnext = require('esnext');
+var applySourceMap = require('vinyl-sourcemaps-apply');
+var objectAssign = require('object-assign');
 
 module.exports = function (options) {
+	var _options = options || {};
+
 	return through.obj(function (file, enc, cb) {
 		if (file.isNull()) {
 			this.push(file);
@@ -16,17 +20,16 @@ module.exports = function (options) {
 		}
 
 		try {
+			var options = objectAssign({}, _options);
+			if (file.sourceMap) {
+				options.sourceFileName = options.sourceFileName || file.relative;
+				options.sourceMapName = options.sourceMapName || file.relative;// + '.map';
+			}
 			var res = esnext.compile(file.contents.toString(), options);
 
 			file.contents = new Buffer(res.code);
-
-			if (res.map) {
-				this.push(new gutil.File({
-					cwd: file.cwd,
-					base: file.base,
-					path: file.path + '.map',
-					contents: new Buffer(res.map)
-				}));
+			if (res.map && file.sourceMap) {
+				applySourceMap(file, res.map);
 			}
 		} catch (err) {
 			this.emit('error', new gutil.PluginError('gulp-esnext', err));
